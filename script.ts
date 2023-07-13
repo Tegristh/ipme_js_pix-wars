92de4026f8a4c8d6a9d31cb9ab9c50f5d6a67eea
Baptiste <63445829+Tegristh@users.noreply.github.com>
2023-07-13 19:04:31 +0200

    Rainbow color picker

--- script.ts
+++ script.ts
@@ -1,6 +1,10 @@
 const colorsList: string[] = [
-    "#FF0000", "#00FF00", "#0000FF", "#000", "#FFF",
-    "#FF0000", "#00FF00", "#0000FF", "#000", "#FFF"
+    "#e71d43", "#ff0000", "#ff3700", "#ff6e00", "#ffa500",
+    "#ffc300", "#ffe100", "#ffff00", "#aad500", "#55aa00",
+    "#008000", "#005555", "#002baa", "#0000ff", "#1900d5",
+    "#3200ac", "#4b0082", "#812ba6", "#b857ca", "#d03a87",
+    "#FFF", "#000"
+    
 ]
 let currentColorChoice: string = colorsList[2]
 
